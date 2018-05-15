7b4f279bcea9dad14e5fd2947660d8ede699f4cc
Israel Aguilar <iaguilaror@gmail.com>
2018-05-14 23:04:11 -0500

    testing email notifications

--- run_test.sh
+++ run_test.sh
@@ -9,7 +9,7 @@
 #nextflow main.nf --small_test --sample "single" --reference "hg19" --strand "unstranded" --ercc --fullCov -profile sge -with-report -with-dag flowchart.png -resume
 
 ## hg38 Simple test with SGE (no docker)
-nextflow main.nf --small_test --sample "single" --reference "hg38" --strand "unstranded" --ercc --fullCov -profile sge -resume -with-report -with-dag flowchart.png
+nextflow main.nf --small_test --sample "single" --reference "hg38" --strand "unstranded" --ercc -profile sge -resume -with-report -with-dag flowchart.png -N iaguilaror@gmail.com
 
 ## mm10 Simple test with SGE, (no docker)
 #nextflow main.nf --small_test --sample "single" --reference "mm10" --strand "unstranded" --ercc --fullCov -profile sge -with-report -with-dag flowchart.png -resume
