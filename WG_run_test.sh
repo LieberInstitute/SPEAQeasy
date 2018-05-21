5eaa6e3d756270e31c486faebfbc1b4fd403b404
Ever891020 <ever.gmillan@gmail.com>
2018-05-21 17:13:37 -0500

    after partially successful testing in hg19,mm10 and rn6

--- WG_run_test.sh
+++ WG_run_test.sh
@@ -9,7 +9,7 @@
 #nextflow main.nf --small_test --sample "single" --reference "hg19" --strand "unstranded" --ercc --fullCov -profile sge -with-report -with-dag flowchart.png -resume
 
 ## hg38 Simple test with SGE (no docker)
-nextflow main.nf --small_test --sample "single" --reference "hg38" --strand "unstranded" --ercc -profile sge -resume -with-report -with-dag flowchart.png -N iaguilaror@gmail.com
+#nextflow main.nf --small_test --sample "single" --reference "hg38" --strand "unstranded" --ercc -profile sge -resume -with-report -with-dag flowchart.png -N iaguilaror@gmail.com
 
 ## mm10 Simple test with SGE, (no docker)
 #nextflow main.nf --small_test --sample "single" --reference "mm10" --strand "unstranded" --ercc --fullCov -profile sge -with-report -with-dag flowchart.png -resume
@@ -23,3 +23,47 @@ nextflow main.nf --small_test --sample "single" --reference "hg38" --strand "uns
 #&& nextflow main.nf --sample "single" --reference "mm10" --strand "unstranded" -with-report -with-dag \
 #&& nextflow main.nf --sample "single" --reference "rn6" --strand "unstranded" -with-report -with-dag
 
+###Testing hg38 paired unstranded
+#nextflow main.nf --small_test --sample "paired" --reference "hg38" --strand "unstranded" --ercc -profile quick -with-report -with-dag flowchart.png
+
+### Testing hg19 paired stranded
+#nextflow main.nf --small_test --sample "paired" --reference "hg19" --strand "reverse" --ercc -profile quick -with-report -with-dag flowchart.png
+### Testing hg19 paired unstranded
+#nextflow main.nf --small_test --sample "paired" --reference "hg19" --strand "unstranded" --ercc -profile quick -with-report -with-dag flowchart.png
+
+### Testing hg19 single stranded
+#nextflow main.nf --small_test --sample "single" --reference "hg19" --strand "forward" --ercc -profile quick -with-report -with-dag flowchart.png
+### Testing hg19 single unstranded
+#nextflow main.nf --small_test --sample "single" --reference "hg19" --strand "unstranded" --ercc -profile quick -with-report -with-dag flowchart.png
+
+
+### Testing hg19 paired stranded
+#nextflow main.nf --small_test --sample "single" --reference "hg19" --strand "forward" --ercc -profile quick -with-report -with-dag flowchart.png
+### Testing mm10 single unstranded
+#nextflow main.nf --small_test --sample "single" --reference "mm10" --strand "unstranded" --ercc -profile quick -with-report -with-dag flowchart.png
+
+
+### Testing mm10 paired unstranded
+#nextflow main.nf --small_test --sample "paired" --reference "mm10" --strand "unstranded" --ercc -profile quick -with-report -with-dag flowchart.png
+
+### Testing mm10 paired stranded
+#nextflow main.nf --small_test --sample "paired" --reference "mm10" --strand "forward" --ercc -profile quick -with-report -with-dag flowchart.png
+
+
+### Testing rna6 single unstranded
+#nextflow main.nf --small_test --sample "single" --reference "rn6" --strand "unstranded" --ercc -profile quick -with-report -with-dag flowchart.png
+
+### Testing rna6 single stranded
+#nextflow main.nf --small_test --sample "single" --reference "rn6" --strand "forward" --ercc -profile quick -with-report -with-dag flowchart.png
+
+### Testing rna6 single stranded
+#nextflow main.nf --small_test --sample "single" --reference "rn6" --strand "reverse" --ercc -profile quick -with-report -with-dag flowchart.png
+
+### Testing rna6 paired stranded
+#nextflow main.nf --small_test --sample "paired" --reference "rn6" --strand "forward" --ercc -profile quick -with-report -with-dag flowchart.png
+
+### Testing rna6 paired stranded
+#nextflow main.nf --small_test --sample "paired" --reference "rn6" --strand "reverse" --ercc -profile quick -with-report -with-dag flowchart.png
+
+### Testing rna6 paired unstranded
+nextflow main.nf --small_test --sample "paired" --reference "rn6" --strand "unstranded" --ercc -profile quick -with-report -with-dag flowchart.png
