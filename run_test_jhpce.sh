049679dd1dd2d2636ae54fc7c0b89c364b9a6998
lcolladotor <lcollado@jhsph.edu>
2018-12-03 15:13:59 -0500

    Add JHPCE test bash scripts. Note that they run in the current working directory. Use the -w option to change the directory where files will run

---
+++ run_test_jhpce.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## hg38 Simple test (no docker, no sge)
+nextflow main.nf \
+	--small_test \
+	--sample "single" \
+	--reference "hg38" \
+	--strand "unstranded" \
+	--ercc \
+	--fullCov \
+	--wg_test \
+	-with-report execution_reports/JHPCE_mode_test_run.html \
+	-with-dag execution_DAGs/JHPCE_mode_test_run.DAG \
+	-profile jhpce,quick \
+	$@
