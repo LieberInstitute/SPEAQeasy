385f347ec79aafd2efd2a466eab292e0cc408209
lcolladotor <lcollado@jhsph.edu>
2018-12-13 11:29:40 -0500

    Use directories at /dcl01/lieber/ajaffe for the annotation, genotyping, results, reports, etc. That way the burden on /users/yourUsername is reduced. I was hitting the 100G limit there already. This wouldn't have been necessary if #12 wasn't a problem. However, it's also a bit cleaner.

--- run_test_jhpce.sh
+++ run_test_jhpce.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+## use qrsh -l mem_free=32G,h_vmem=32G,h_fsize=100G
+module load nextflow
+export _JAVA_OPTIONS="-Xms25g -Xmx30g"
+
+# The annotation files were downloaded from https://github.com/LieberInstitute/RNAsp/
+# specifically from https://github.com/LieberInstitute/RNAsp/tree/438d25b44fef422f67865e4a16f941d50d173399
+# and then copied to their location:
+#
+# git clone git@github.com:LieberInstitute/RNAsp.git
+# cd RNAsp
+# mv Annotation /dcl01/lieber/ajaffe/lab/RNAsp_static/
+# mv Genotyping /dcl01/lieber/ajaffe/lab/RNAsp_static/
+
+
 ## hg38 Simple test (no docker, no sge)
 nextflow main.nf \
 	--small_test \
@@ -9,7 +23,13 @@ nextflow main.nf \
 	--ercc \
 	--fullCov \
 	--wg_test \
-	-with-report execution_reports/JHPCE_mode_test_run.html \
-	-with-dag execution_DAGs/JHPCE_mode_test_run.DAG \
+	-with-report /dcl01/lieber/ajaffe/lab/RNAsp_work/execution_reports/JHPCE_mode_test_run.html \
+	-with-dag /dcl01/lieber/ajaffe/lab/RNAsp_work/execution_DAGs/JHPCE_mode_test_run.DAG \
 	-profile jhpce,quick \
+    -w "/dcl01/lieber/ajaffe/lab/RNAsp_work/runs" \
+    --annotation "/dcl01/lieber/ajaffe/lab/RNAsp_static/Annotation" \
+    --indexing "/dcl01/lieber/ajaffe/lab/RNAsp_static/Annotation" \
+    --genotype "/dcl01/lieber/ajaffe/lab/RNAsp_static/Genotyping" \
+    --output "/dcl01/lieber/ajaffe/lab/RNAsp_work/results" \
 	$@
+
