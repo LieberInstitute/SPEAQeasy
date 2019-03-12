bbfcd132a8e220a8ba9298640bf1fef69528e84a
Nick-Eagles <nick.eagles@libd.org>
2019-03-12 13:05:53 -0400

    Small change in syntax required for GenomeInfoDb package in R/3.5; meant to include this in previous commit

--- scripts/create_count_objects-mouse.R
+++ scripts/create_count_objects-mouse.R
@@ -387,7 +387,7 @@ juncCounts = lapply(juncCounts, function(x) x[jIndex,])
 
 ############ anno/jMap
 anno = juncCounts$anno
-seqlevels(anno, force=TRUE) = paste0("chr", c(1:19,"X","Y","M"))
+seqlevels(anno, pruning.mode="coarse") = paste0("chr", c(1:19,"X","Y","M"))
 
 ## add additional annotation
 anno$inGencode = countOverlaps(anno, theJunctions, type="equal") > 0
