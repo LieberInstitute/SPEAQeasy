a82f576e599587f69b67dbad3684996336970f9c
Nick-Eagles <nick.eagles@libd.org>
2019-07-22 17:34:10 -0400

    More updates to jhpce config specification, notably changes to time limit and h_fsize, required for real runs on larger samples. Java VM memory settings were lowered, as errors were resulting from java demanding access to whatever memory was allocated (regardless of the amount needed)

--- run_test_jhpce.sh
+++ run_test_jhpce.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-## use qrsh -l mem_free=32G,h_vmem=32G,h_fsize=100G
+## use 'qrsh -l mem_free=60G,h_vmem=60G,h_fsize=100G' for real runs
 module load nextflow
-export _JAVA_OPTIONS="-Xms25g -Xmx30g"
+export _JAVA_OPTIONS="-Xms8g -Xmx10g"
 
 # The annotation files were downloaded from https://github.com/LieberInstitute/RNAsp/
 # specifically from https://github.com/LieberInstitute/RNAsp/tree/438d25b44fef422f67865e4a16f941d50d173399
@@ -13,23 +13,18 @@ export _JAVA_OPTIONS="-Xms25g -Xmx30g"
 # mv Annotation /dcl01/lieber/ajaffe/lab/RNAsp_static/
 # mv Genotyping /dcl01/lieber/ajaffe/lab/RNAsp_static/
 
-
-## hg38 Simple test (no docker, no sge)
 nextflow main.nf \
-	--small_test \
 	--sample "single" \
-	--reference "hg38" \
+	--reference "hg19" \
 	--strand "unstranded" \
 	--ercc \
-	--fullCov \
-	--wg_test \
 	-with-report /dcl01/lieber/ajaffe/lab/RNAsp_work/execution_reports/JHPCE_mode_test_run.html \
 	-with-dag /dcl01/lieber/ajaffe/lab/RNAsp_work/execution_DAGs/JHPCE_mode_test_run.DAG \
-	-profile jhpce,quick \
-    -w "/dcl01/lieber/ajaffe/lab/RNAsp_work/runs" \
-    --annotation "/dcl01/lieber/ajaffe/lab/RNAsp_static/Annotation" \
-    --indexing "/dcl01/lieber/ajaffe/lab/RNAsp_static/Annotation" \
-    --genotype "/dcl01/lieber/ajaffe/lab/RNAsp_static/Genotyping" \
-    --output "/dcl01/lieber/ajaffe/lab/RNAsp_work/results" \
+	-profile jhpce  \
+  -w "/dcl01/lieber/ajaffe/lab/RNAsp_work/runs" \
+  --annotation "/dcl01/lieber/ajaffe/lab/RNAsp_static/Annotation" \
+  --indexing "/dcl01/lieber/ajaffe/lab/RNAsp_static/Annotation" \
+  --genotype "/dcl01/lieber/ajaffe/lab/RNAsp_static/Genotyping" \
+  --output "/dcl01/lieber/ajaffe/lab/RNAsp_work/results" \
 	$@
 
