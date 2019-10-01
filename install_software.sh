915f996367c1b2b6b859897626dc6dc11bac6e27
Nick-Eagles <nick.eagles@libd.org>
2019-10-01 15:34:02 -0400

    Fix installation of RSeQC (use the python 2.7.9 that is installed to set it up, and install dependent packages); fix regtools installation (install cmake and use this to install regtools); make organizational improvements

--- install_software.sh
+++ install_software.sh
@@ -8,7 +8,7 @@ INSTALL_DIR=$(pwd)
 
 #  To do: consider how to handle java, R 3.6 and its packages, ucsctools/wigToBigWig, wiggletools
 
-#  bcftools (1.9)
+#  bcftools (1.9)  -------------------------------------------------------------
 
 wget https://github.com/samtools/bcftools/releases/download/1.9/bcftools-1.9.tar.bz2 -O bcftools.tar.bz2 && \
     tar -xjvf bcftools.tar.bz2 && \
@@ -17,19 +17,19 @@ wget https://github.com/samtools/bcftools/releases/download/1.9/bcftools-1.9.tar
     make && \
     make install
     
-#  fastqc (0.11.5)
+#  fastqc (0.11.5)  -------------------------------------------------------------
 
 wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.5.zip && \
   unzip fastqc_v0.11.5.zip && \
   chmod -R 755 FastQC
   
-#  hisat2 (2.1.0)
+#  hisat2 (2.1.0)  -------------------------------------------------------------
 
 wget ftp://ftp.ccb.jhu.edu/pub/infphilo/hisat2/downloads/hisat2-2.1.0-Linux_x86_64.zip && \
   unzip hisat2-2.1.0-Linux_x86_64.zip && \
   chmod -R 755 hisat2-2.1.0
   
-#  htslib (1.9)
+#  htslib (1.9)  -------------------------------------------------------------
 
 wget https://github.com/samtools/htslib/releases/download/1.9/htslib-1.9.tar.bz2 -O htslib-1.9.tar.bz2 && \
     tar -xjvf htslib-1.9.tar.bz2 && \
@@ -38,20 +38,37 @@ wget https://github.com/samtools/htslib/releases/download/1.9/htslib-1.9.tar.bz2
     make && \
     make install
 
-#  kallisto (0.43.0)
+#  kallisto (0.43.0)  -------------------------------------------------------------
 
 wget https://github.com/pachterlab/kallisto/releases/download/v0.43.0/kallisto_linux-v0.43.0.tar.gz && \
   tar xzvf kallisto_linux-v0.43.0.tar.gz && \
   chmod -R 755 kallisto_linux-v0.43.0
   
-#  nextflow (latest)
+#  nextflow (latest)  -------------------------------------------------------------
 
 cd ..
 wget -qO- https://get.nextflow.io | bash
 cd Software
 
-#  python (2.7.9)
+#  regtools (0.3.0)  -------------------------------------------------------------
 
+#  cmake, which regtools needs to build
+wget https://github.com/Kitware/CMake/releases/download/v3.14.6/cmake-3.14.6-Linux-x86_64.tar.gz && \
+  tar xzvf cmake-3.14.6-Linux-x86_64.tar.gz
+
+#  regtools itself
+wget https://github.com/griffithlab/regtools/archive/0.3.0.tar.gz -O regtools-0.3.0.tar.gz && \
+    tar -xvf regtools-0.3.0.tar.gz &&\
+    cd regtools-0.3.0 && \
+    mkdir build && \
+    cd build && \
+    ../../cmake-3.14.6-Linux-x86_64/bin/cmake .. && \
+    make
+    cd ../..
+    
+#  rseqc (2.6.4)  -------------------------------------------------------------
+
+#  Python (2.7.9), which RSeQC uses
 wget https://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz && \
   tar xzvf Python-2.7.9.tgz && \
   chmod -R 755 Python-2.7.9 && \
@@ -60,31 +77,23 @@ wget https://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz && \
   make && \
   make install
   cd ..
+  
+#  Python packages RSeQC depends on, but does not install
+$INSTALL_DIR/Python-2.7.9/build/python -m pip install --user RSeQC==2.6.4
 
-#  regtools (0.3.0)
-
-wget https://github.com/griffithlab/regtools/archive/0.3.0.tar.gz -O regtools-0.3.0.tar.gz && \
-    tar -xvf regtools-0.3.0.tar.gz &&\
-    cd regtools-0.3.0 && \
-    mkdir build && \
-    cd build && \
-    cmake .. && \
-    make
-    
-#  rseqc (2.6.4)
-
+#  RSeQC itself, set up via Python
 wget https://downloads.sourceforge.net/project/rseqc/RSeQC-2.6.4.tar.gz && \
   tar zxf RSeQC-2.6.4.tar.gz && \
   cd RSeQC-2.6.4 && \
-  python setup.py install --root=bin
+  $INSTALL_DIR/Python-2.7.9/build/python setup.py install --root=bin
     
-#  salmon (0.8.2)
+#  salmon (0.8.2)  -------------------------------------------------------------
 
 wget https://github.com/COMBINE-lab/salmon/releases/download/v0.8.2/Salmon-0.8.2_linux_x86_64.tar.gz && \
   tar xzvf Salmon-0.8.2_linux_x86_64.tar.gz && \
   chmod -R 755 Salmon-0.8.2_linux_x86_64
   
-#  samtools (1.9)
+#  samtools (1.9)  -------------------------------------------------------------
 
 wget https://github.com/samtools/samtools/releases/download/1.9/samtools-1.9.tar.bz2 -O samtools.tar.bz2 && \
     tar -xjvf samtools.tar.bz2 && \
@@ -93,19 +102,19 @@ wget https://github.com/samtools/samtools/releases/download/1.9/samtools-1.9.tar
     make && \
     make install
     
-#  subread/ featureCounts (1.5.0-p3)
+#  subread/ featureCounts (1.5.0-p3)  -------------------------------------------------------------
 
 wget https://sourceforge.net/projects/subread/files/subread-1.5.0-p3/subread-1.5.0-p3-Linux-x86_64.tar.gz/download && \
   tar xzvf download && \
   chmod -R 755 subread-1.5.0-p3-Linux-x86_64
   
-#  trimmomatic (0.36)
+#  trimmomatic (0.36)  -------------------------------------------------------------
 
 wget http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.36.zip && \
   unzip Trimmomatic-0.36.zip && \
   chmod -R 755 Trimmomatic-0.36
   
-#  wiggletools (1.2.1)
+#  wiggletools (1.2.1)  -------------------------------------------------------------
 
 mkdir wiggle
 cd wiggle
