b4fed78aba22be71e73bf35605be2fb870c79102
Nick-Eagles <nick.eagles@libd.org>
2019-11-25 15:00:10 -0500

    Updated local execution script: main changes were lowering Java memory usage and using consistent formatting

--- run_test_system.sh
+++ run_test_system.sh
@@ -2,15 +2,16 @@
 
 ## Test using local hardware resources and software pointed to
 ## in conf/command_paths_long.config
-export _JAVA_OPTIONS="-Xms8g -Xmx10g"
+export _JAVA_OPTIONS="-Xms5g -Xmx7g"
 ./Software/nextflow main.nf \
-	--small_test \
-	--sample "single" \
-	--reference "hg38" \
-	--strand "unstranded" \
-	--ercc \
-	--fullCov \
-	-with-report execution_reports/System_mode_test_run.html \
-  -with-dag execution_DAGs/System_mode_test_run.png \
-	-profile standard \
-	$@
+    --small_test \
+    --sample "single" \
+    --reference "hg38" \
+    --strand "unstranded" \
+    --ercc \
+    --fullCov \
+    --force_trim \
+    -with-report execution_reports/System_mode_test_run.html \
+    -with-dag execution_DAGs/System_mode_test_run.png \
+    -profile standard \
+    $@
