3cb4a91c9eea0fb5f80fa3419f18cab629d0644d
Nick-Eagles <nick.eagles@libd.org>
2019-12-12 17:33:01 -0500

    Remove unused code

--- scripts/infer_strand.R
+++ scripts/infer_strand.R
@@ -22,28 +22,16 @@ r_frac = opt$reverse / (opt$forward + opt$reverse)
 
 if (1 - f_frac < thres) {
     strandness = "reverse"
-    if (opt$paired == "paired") {
-        strand_code = "1++,1--,2+-,2-+"
-    } else {
-        strand_code = "++,--"
-    }
     print(paste0("Greater than ", round(100 * (1 - thres), 2), "% of pseudoaligned reads matches forward-strandness."))
 } else if (1 - r_frac < thres) {
     strandness = "reverse"
-    if (opt$paired == "paired") {
-        strand_code = "1+-,1-+,2++,2--"
-    } else {
-        strand_code = "+-,-+"
-    }
     print(paste0("Greater than ", round(100 * (1 - thres), 2), "% of pseudoaligned reads matches reverse-strandness."))
 } else if (abs(f_frac - 0.5) < thres) {
-    strand_code = "unstranded"
     strandness = "unstranded"
     print(paste0(round(100 * f_frac, 2), "% of pseudoaligned reads match forward-strandness;"))
     print(paste0(round(100 * r_frac, 2), "% of pseudoaligned reads match reverse-strandness."))
     print(paste0("The larger percentage is no more than ", round(100 * (0.5 + thres), 2), "%, thus the reads appear unstranded."))
 } else {
-    strand_code = "unstranded"
     strandness = "unstranded"
     print("[Warning] Based on psuedo-alignment testing, reads do not conclusively appear stranded nor unstranded.")
     print(paste0("[Warning] Observed ratios: ", round(f_frac, 3), " forward; ", round(r_frac, 3), " reverse. Threshold used: ", thres, "."))
