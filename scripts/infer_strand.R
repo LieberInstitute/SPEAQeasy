9039259105ddf29f77c4f70aced21cf0468c2712
Nick-Eagles <nick.eagles@libd.org>
2019-12-17 11:42:26 -0500

    Allow different sensitivities for determining unstrandedness and determining strandedness; update log output accordingly

--- scripts/infer_strand.R
+++ scripts/infer_strand.R
@@ -1,9 +1,8 @@
 library('getopt')
 
-# A value below 0.25, determining how close to 1 or 0.5 the larger ratio
-# (reverse or forward) must be to conclude it is stranded or unstranded,
-# respectively.
-thres = 0.15
+#  These must sum to <= 0.5
+thres_strand = 0.2   # How close to 1 the larger ratio (reverse or forward) must be to conclude strandedness
+thres_unstrand = 0.1 # How close to 0.5 either ratio must be to be considered unstranded
 
 spec <- matrix(c(
     'forward', 'f', 1, 'numeric', 'forward count of psuedo-aligned reads',
@@ -20,21 +19,24 @@ r_frac = opt$reverse / (opt$forward + opt$reverse)
 #  Determine the apparent strandness
 #######################################################################
 
-if (1 - f_frac < thres) {
+if (1 - f_frac < thres_strand) {
     strandness = "forward"
-    print(paste0("Greater than ", round(100 * (1 - thres), 2), "% of pseudoaligned reads matches forward-strandness."))
-} else if (1 - r_frac < thres) {
+    print(paste0("Greater than ", round(100 * (1 - thres_strand), 2), "% of pseudoaligned reads matches forward-strandness."))
+} else if (1 - r_frac < thres_strand) {
     strandness = "reverse"
-    print(paste0("Greater than ", round(100 * (1 - thres), 2), "% of pseudoaligned reads matches reverse-strandness."))
-} else if (abs(f_frac - 0.5) < thres) {
+    print(paste0("Greater than ", round(100 * (1 - thres_strand), 2), "% of pseudoaligned reads matches reverse-strandness."))
+} else if (abs(f_frac - 0.5) < thres_unstrand) {
     strandness = "unstranded"
     print(paste0(round(100 * f_frac, 2), "% of pseudoaligned reads match forward-strandness;"))
     print(paste0(round(100 * r_frac, 2), "% of pseudoaligned reads match reverse-strandness."))
-    print(paste0("The larger percentage is no more than ", round(100 * (0.5 + thres), 2), "%, thus the reads appear unstranded."))
+    print(paste0("The larger percentage is no more than ", round(100 * (0.5 + thres_unstrand), 2), "%, thus the reads appear unstranded."))
 } else {
     strandness = "unstranded"
     print("[Warning] Based on psuedo-alignment testing, reads do not conclusively appear stranded nor unstranded.")
-    print(paste0("[Warning] Observed ratios: ", round(f_frac, 3), " forward; ", round(r_frac, 3), " reverse. Threshold used: ", thres, "."))
+    print(paste0("[Warning] Observed ratios: ", round(f_frac, 3), " forward; ", round(r_frac, 3), " reverse. Thresholds used:"))
+    print(paste0("              Larger ratio > ", round(100 * (1 - thres_strand), 2), "% constitutes stranded reads;"))
+    print(paste0("              Ratios in [", round(100 * (0.5 - thres_unstrand ), 2), ", ", round(100 * (0.5 + thres_unstrand ), 2),
+                 "]% constitute unstrandedness."))
 }
 
 #######################################################################
