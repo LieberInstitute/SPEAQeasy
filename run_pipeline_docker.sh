24ad1f190d3bb559d0079d9d3d5c8f478e7e0820
Nick-Eagles <nick.eagles@libd.org>
2020-01-22 15:37:13 -0500

    Miscellaneous fixes to get docker mode running

--- run_pipeline_docker.sh
+++ run_pipeline_docker.sh
@@ -3,15 +3,14 @@
 ## Execution using docker to manage required software, and currently
 ## configured to use local hardware resources.
 export _JAVA_OPTIONS="-Xms8g -Xmx10g"
-nextflow main.nf \
+Software/nextflow main.nf \
 	--small_test \
 	--sample "single" \
 	--reference "hg38" \
 	--strand "unstranded" \
 	--ercc \
 	--fullCov \
-	-with-report execution_reports/Docker_mode_test_run.html \
-	-with-dag execution_DAGs/Docker_mode_test_run.png \
-	-resume \
-	-profile docker,quick \
+	-with-report execution_reports/docker_run.html \
+	-with-dag execution_DAGs/docker_run.html \
+	-profile docker \
 	$@
