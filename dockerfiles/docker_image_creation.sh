f7b5ad164c015b10f03dc603d80947599a64bf9b
Nick-Eagles <nick.eagles@libd.org>
2020-01-23 16:36:52 -0500

    Add Dockerfiles for R+bioc packages and R+bioc packages+kallisto (InferStrandness); add a bash script to document exactly how these (and future docker images) were built

---
+++ dockerfiles/docker_image_creation.sh
@@ -0,0 +1,46 @@
+#  This script contains all the code used to build the current docker images,
+#  for reproducibility.
+
+#  The user's working directory should be [REPO]/dockerfiles/
+REPO=..
+
+########################################################
+#  R with bioconductor packages pre-installed
+########################################################
+
+mkdir $REPO/r_3.6.1_bioc
+cp $REPO/scripts/check_R_packages.R $REPO/r_3.6.1_bioc/
+#  produce $REPO/r_3.6.1_bioc/Dockerfile
+docker pull bioconductor/bioconductor_full:RELEASE_3_10
+docker build -t libddocker/r_3.6.1_bioc $REPO/r_3.6.1_bioc
+docker push libddocker/r_3.6.1_bioc
+
+########################################################
+#  R + bioconductor packages + kallisto
+########################################################
+
+mkdir $REPO/infer_strandness
+#  produce $REPO/infer_strandness/Dockerfile
+docker pull libddocker/r_3.6.1_bioc
+docker build -t libddocker/infer_strandness $REPO/infer_strandness
+docker push libddocker/infer_strandness
+
+########################################################
+#  regtools (0.5.1)
+########################################################
+
+mkdir -p $REPO/regtools/0.5.1
+# produce $REPO/regtools/0.5.1/Dockerfile
+docker pull libddocker/ubuntu16.04_base:latest
+docker build -t libddocker/regtools:0.5.1 $REPO/regtools/0.5.1
+docker push libddocker/regtools:0.5.1
+
+########################################################
+#  samtools (1.9)
+########################################################
+
+mkdir -p $REPO/samtools/1.9
+#  produce $REPO/samtools/1.9/Dockerfile
+docker pull libddocker/ubuntu16.04_base:latest
+docker build -t libddocker/samtools:1.9 $REPO/samtools/1.9
+docker push libddocker/samtools:1.9
