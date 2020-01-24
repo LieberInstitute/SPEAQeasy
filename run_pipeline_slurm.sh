9efef20f4ba11b1fe77ad511be37f485212dd137
Nick-Eagles <nick.eagles@libd.org>
2020-01-24 12:03:22 -0500

    Add the main script for slurm runs. Closes #47

---
+++ run_pipeline_slurm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export _JAVA_OPTIONS="-Xms8g -Xmx10g"
+
+Software/nextflow main.nf \
+    --sample "single" \
+    --reference "hg19" \
+    --strand "unstranded" \
+    --small_test \
+    -with-report execution_reports/pipeline_report.html \
+    -with-dag execution_DAGs/pipeline_DAG.html \
+    -profile slurm
