fc57b6ec3cb43321973f133a1b007ff703a48282
Nick-Eagles <nick.eagles@libd.org>
2020-07-07 12:36:58 -0400

    Properly point wiggletools to the existing libraries

--- scripts/fix_makefile.R
+++ scripts/fix_makefile.R
@@ -11,13 +11,17 @@ getLineNum = function(key) {
     match(TRUE, isLine)
 }
 
-#  Point to the gsl headers
+#  GSL and libBigWig were installed to a local directory, which the
+#  Makefile needs to be aware of
 incLine = getLineNum("INC=")
 makefile[incLine] = paste(makefile[incLine], "-I../../include")
 
+libLine = getLineNum("LIB_PATHS=")
+makefile[libLine] = paste(makefile[libLine], "-L../../lib")
+
 #  Add the additional required libraries to the command-line options
 libLine = getLineNum("LIBS=")
-makefile[libLine] = paste(makefile[libLine], "-lcrypto -llzma -lbz2")
+makefile[libLine] = paste(makefile[libLine], "-lcrypto -lbz2")
 
 #  Rewrite the Makefile in the same location
 writeLines(makefile, makefilePath)
