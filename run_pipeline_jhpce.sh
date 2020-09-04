8ab30ffdd9a7abeb73cb4f937edfccc7b6912ca6
Nick-Eagles <nick.eagles@libd.org>
2020-09-04 16:22:37 -0400

    Update the original JHPCE repository name to 'SPEAQeasy' from the old 'RNAsp'

--- run_pipeline_jhpce.sh
+++ run_pipeline_jhpce.sh
@@ -7,13 +7,13 @@
 module load nextflow
 export _JAVA_OPTIONS="-Xms8g -Xmx10g"
 
-nextflow /dcl01/lieber/ajaffe/Nick/RNAsp/main.nf \
+nextflow /dcl01/lieber/ajaffe/Nick/SPEAQeasy/main.nf \
     --sample "paired" \
     --reference "hg38" \
     --strand "forward" \
     --small_test \
     --trim_mode "force" \
-    --annotation "/dcl01/lieber/ajaffe/Nick/RNAsp/Annotation" \
+    --annotation "/dcl01/lieber/ajaffe/Nick/SPEAQeasy/Annotation" \
     -with-report execution_reports/JHPCE_run.html \
     -profile jhpce \
     -w "/dcl01/lieber/ajaffe/lab/RNAsp_work/runs" \
@@ -26,4 +26,4 @@ nextflow /dcl01/lieber/ajaffe/Nick/RNAsp/main.nf \
 #  section, and so if you rename the log, you must also pass replace the filename
 #  in the bash call below.
 echo "Generating per-sample logs for debugging..."
-bash /dcl01/lieber/ajaffe/Nick/RNAsp/scripts/generate_logs.sh $PWD/SPEAQeasy_output.log
+bash /dcl01/lieber/ajaffe/Nick/SPEAQeasy/scripts/generate_logs.sh $PWD/SPEAQeasy_output.log
