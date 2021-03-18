3e8cc287f81d93f75b2f1f670925fc91e2c78f4e
Nick-Eagles <nick.eagles@libd.org>
2021-03-18 16:16:01 -0400

    Remove use of '&&' from the entire installation script

--- install_software.sh
+++ install_software.sh
@@ -68,89 +68,89 @@ elif [ "$1" == "local" ]; then
         wget -qO- https://get.nextflow.io | bash 
     
         #  bc (1.06.95)
-        wget ftp://alpha.gnu.org/gnu/bc/bc-1.06.95.tar.bz2 && \
-            tar -xjf bc-1.06.95.tar.bz2 && \
-            cd bc-1.06.95 && \
-            ./configure --prefix=$INSTALL_DIR && \
-            make && \
-            make install
-            cd $INSTALL_DIR
+        wget ftp://alpha.gnu.org/gnu/bc/bc-1.06.95.tar.bz2
+        tar -xjf bc-1.06.95.tar.bz2
+        cd bc-1.06.95
+        ./configure --prefix=$INSTALL_DIR
+        make
+        make install
+        cd $INSTALL_DIR
         
         #  bcftools (1.10.2)  -------------------------------------------------------------
     
-        wget https://github.com/samtools/bcftools/releases/download/1.10.2/bcftools-1.10.2.tar.bz2 -O bcftools.tar.bz2 && \
-            tar -xjf bcftools.tar.bz2 && \
-            cd bcftools-1.10.2 && \
-            ./configure prefix=$INSTALL_DIR && \
-            make && \
-            make install
-            cd $INSTALL_DIR
+        wget https://github.com/samtools/bcftools/releases/download/1.10.2/bcftools-1.10.2.tar.bz2 -O bcftools.tar.bz2
+        tar -xjf bcftools.tar.bz2
+        cd bcftools-1.10.2
+        ./configure prefix=$INSTALL_DIR
+        make
+        make install
+        cd $INSTALL_DIR
             
         #  cmake, which regtools and salmon need to build
-        git clone https://gitlab.kitware.com/cmake/cmake.git && \
-            cd cmake && \
-            ./bootstrap --prefix=$INSTALL_DIR && \
-            make && \
-            make install
-            cd $INSTALL_DIR
+        git clone https://gitlab.kitware.com/cmake/cmake.git
+        cd cmake
+        ./bootstrap --prefix=$INSTALL_DIR
+        make
+        make install
+        cd $INSTALL_DIR
         
         #  fastqc (0.11.8)  -------------------------------------------------------------
     
-        wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.8.zip && \
-            unzip fastqc_v0.11.8.zip && \
-            chmod -R 775 FastQC
+        wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.8.zip
+        unzip fastqc_v0.11.8.zip
+        chmod -R 775 FastQC
       
         #  hisat2 (2.2.1)  -------------------------------------------------------------
     
-        wget https://github.com/DaehwanKimLab/hisat2/archive/v2.2.1.tar.gz && \
-            tar -xzf v2.2.1.tar.gz && \
-            cd hisat2-2.2.1 && \
-            make
-            cd $INSTALL_DIR
+        wget https://github.com/DaehwanKimLab/hisat2/archive/v2.2.1.tar.gz
+        tar -xzf v2.2.1.tar.gz
+        cd hisat2-2.2.1
+        make
+        cd $INSTALL_DIR
         
         #  htslib (1.10.2)  -------------------------------------------------------------
     
-        wget https://github.com/samtools/htslib/releases/download/1.10.2/htslib-1.10.2.tar.bz2 -O htslib.tar.bz2 && \
-            tar -xjf htslib.tar.bz2 && \
-            cd htslib-1.10.2 && \
-            ./configure prefix=$INSTALL_DIR && \
-            make && \
-            make install
-            cd $INSTALL_DIR
-            mv htslib-1.10.2 htslib # wiggletools expects a "plain" name from htslib
+        wget https://github.com/samtools/htslib/releases/download/1.10.2/htslib-1.10.2.tar.bz2 -O htslib.tar.bz2
+        tar -xjf htslib.tar.bz2
+        cd htslib-1.10.2
+        ./configure prefix=$INSTALL_DIR
+        make
+        make install
+        cd $INSTALL_DIR
+        mv htslib-1.10.2 htslib # wiggletools expects a "plain" name from htslib
             
         #  HDF5 (1.10.5), needed for build of kallisto -------------------------------
         
-        wget https://support.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.10.5.tar.gz && \
-            tar -xzf hdf5-1.10.5.tar.gz && \
-            cd hdf5-1.10.5 && \
-            ./configure --disable-parallel --without-szlib --without-pthread --prefix=$INSTALL_DIR && \
-            make && \
-            make install
-            cd $INSTALL_DIR
+        wget https://support.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.10.5.tar.gz
+        tar -xzf hdf5-1.10.5.tar.gz
+        cd hdf5-1.10.5
+        ./configure --disable-parallel --without-szlib --without-pthread --prefix=$INSTALL_DIR
+        make
+        make install
+        cd $INSTALL_DIR
     
         #  kallisto (0.46.1)  -------------------------------------------------------------
                 
-        wget https://github.com/pachterlab/kallisto/archive/v0.46.1.tar.gz && \
-            tar -xzf v0.46.1.tar.gz && \
-            cd kallisto-0.46.1 && \
-            mkdir build && \
-            cd build && \
-            $INSTALL_DIR/bin/cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR .. && \
-            make && \
-            make prefix=$INSTALL_DIR install
-            cd $INSTALL_DIR
+        wget https://github.com/pachterlab/kallisto/archive/v0.46.1.tar.gz
+        tar -xzf v0.46.1.tar.gz
+        cd kallisto-0.46.1
+        mkdir build
+        cd build
+        $INSTALL_DIR/bin/cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR ..
+        make
+        make prefix=$INSTALL_DIR install
+        cd $INSTALL_DIR
     
         #  R (3.6.1) ---------------------------------------------------------------------
         
         #  Install R
-        wget http://cran.rstudio.com/src/base/R-3/R-3.6.1.tar.gz && \
-            tar -xf R-3.6.1.tar.gz && \
-            cd R-3.6.1 && \
-            ./configure --prefix=$INSTALL_DIR && \
-            make && \
-            make install
-            cd $INSTALL_DIR
+        wget http://cran.rstudio.com/src/base/R-3/R-3.6.1.tar.gz
+        tar -xf R-3.6.1.tar.gz
+        cd R-3.6.1
+        ./configure --prefix=$INSTALL_DIR
+        make
+        make install
+        cd $INSTALL_DIR
       
         #  Install packages that will be used by the pipeline
         ./R-3.6.1/bin/Rscript ../scripts/check_R_packages.R
@@ -162,14 +162,14 @@ elif [ "$1" == "local" ]; then
     
         #  regtools (0.5.1)  -------------------------------------------------------------
     
-        wget https://github.com/griffithlab/regtools/archive/0.5.1.tar.gz -O regtools-0.5.1.tar.gz && \
-            tar -xf regtools-0.5.1.tar.gz &&\
-            cd regtools-0.5.1 && \
-            mkdir build && \
-            cd build && \
-            $INSTALL_DIR/bin/cmake .. && \
-            make
-            cd $INSTALL_DIR
+        wget https://github.com/griffithlab/regtools/archive/0.5.1.tar.gz -O regtools-0.5.1.tar.gz
+        tar -xf regtools-0.5.1.tar.gz
+        cd regtools-0.5.1
+        mkdir build
+        cd build
+        $INSTALL_DIR/bin/cmake ..
+        make
+        cd $INSTALL_DIR
         
         #  rseqc (3.0.1)  -------------------------------------------------------------
 
@@ -179,24 +179,24 @@ elif [ "$1" == "local" ]; then
         
         #  salmon (1.2.1)  -------------------------------------------------------------
             
-        wget https://github.com/COMBINE-lab/salmon/archive/v1.2.1.tar.gz && \
-            tar -xzf v1.2.1.tar.gz && \
-            mkdir build && \
-            cd build && \
-            $INSTALL_DIR/bin/cmake -DFETCH_BOOST=TRUE .. && \
-            make && \
-            make install
-            cd $INSTALL_DIR
+        wget https://github.com/COMBINE-lab/salmon/archive/v1.2.1.tar.gz
+        tar -xzf v1.2.1.tar.gz
+        mkdir build
+        cd build
+        $INSTALL_DIR/bin/cmake -DFETCH_BOOST=TRUE ..
+        make
+        make install
+        cd $INSTALL_DIR
       
         #  samtools (1.10)  -------------------------------------------------------------
     
-        wget https://github.com/samtools/samtools/releases/download/1.10/samtools-1.10.tar.bz2 -O samtools.tar.bz2 && \
-            tar -xjf samtools.tar.bz2 && \
-            cd samtools-1.10 && \
-            ./configure prefix=$INSTALL_DIR && \
-            make && \
-            make install
-            cd $INSTALL_DIR
+        wget https://github.com/samtools/samtools/releases/download/1.10/samtools-1.10.tar.bz2 -O samtools.tar.bz2
+        tar -xjf samtools.tar.bz2
+        cd samtools-1.10
+        ./configure prefix=$INSTALL_DIR
+        make && \
+        make install
+        cd $INSTALL_DIR
         
         #  STAR (2.7.8a)  ---------------------------------------------------------------
             
@@ -228,59 +228,59 @@ elif [ "$1" == "local" ]; then
             makefile_name="Makefile.FreeBSD"
         fi
         
-        wget https://sourceforge.net/projects/subread/files/subread-2.0.0/subread-2.0.0-source.tar.gz/download && \
-            tar -zxf download && \
-            cd subread-2.0.0-source/src && \
-            make -f $makefile_name
-            cd $INSTALL_DIR
+        wget https://sourceforge.net/projects/subread/files/subread-2.0.0/subread-2.0.0-source.tar.gz/download
+        tar -zxf download
+        cd subread-2.0.0-source/src
+        make -f $makefile_name
+        cd $INSTALL_DIR
           
         #  trimmomatic (0.39)  -------------------------------------------------------------
         
-        wget http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.39.zip && \
-            unzip Trimmomatic-0.39.zip && \
-            chmod -R 775 Trimmomatic-0.39
+        wget http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.39.zip
+        unzip Trimmomatic-0.39.zip
+        chmod -R 775 Trimmomatic-0.39
           
         #  wiggletools (1.2.1)  -------------------------------------------------------------
         
         ## Install gsl
-        wget ftp://www.mirrorservice.org/sites/ftp.gnu.org/gnu/gsl/gsl-latest.tar.gz && \
-            tar -xf gsl-latest.tar.gz && \
-            cd gsl-2.6 && \
-            ./configure --prefix=$INSTALL_DIR && \
-            make && \
-            make install
-            cd $INSTALL_DIR
-            mv gsl-2.6 gsl # wiggletools expects a "plain" name from gsl
+        wget ftp://www.mirrorservice.org/sites/ftp.gnu.org/gnu/gsl/gsl-latest.tar.gz
+        tar -xf gsl-latest.tar.gz
+        cd gsl-2.6
+        ./configure --prefix=$INSTALL_DIR
+        make
+        make install
+        cd $INSTALL_DIR
+        mv gsl-2.6 gsl # wiggletools expects a "plain" name from gsl
             
         ## Install libBigWig
-        git clone git@github.com:dpryan79/libBigWig.git && \
-            cd libBigWig && \
-            make prefix=$INSTALL_DIR install
-            cd $INSTALL_DIR
+        git clone git@github.com:dpryan79/libBigWig.git
+        cd libBigWig
+        make prefix=$INSTALL_DIR install
+        cd $INSTALL_DIR
         
         # wiggletools itself (note the modified Makefile)
-        git clone git@github.com:Ensembl/WiggleTools.git && \
-            ./R-3.6.1/bin/Rscript ../scripts/fix_makefile.R && \
-            cd WiggleTools && \
-            make prefix=$INSTALL_DIR
-            cd $INSTALL_DIR
+        git clone git@github.com:Ensembl/WiggleTools.git
+        ./R-3.6.1/bin/Rscript ../scripts/fix_makefile.R
+        cd WiggleTools
+        make prefix=$INSTALL_DIR
+        cd $INSTALL_DIR
     
         #  wigToBigWig -----------------------------------------------------------
         
         if [ $(uname -s) == "Linux" ]; then
-            wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/wigToBigWig && \
-                mv wigToBigWig bin/
+            wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/wigToBigWig
+            mv wigToBigWig bin/
         elif [ $(uname -s) == "Darwin" ]; then
-            wget http://hgdownload.soe.ucsc.edu/admin/exe/macOSX.x86_64/wigToBigWig && \
-                mv wigToBigWig bin/
+            wget http://hgdownload.soe.ucsc.edu/admin/exe/macOSX.x86_64/wigToBigWig
+            mv wigToBigWig bin/
         else
             #  Attempt to build from source without assuming the OS
-            wget http://hgdownload.cse.ucsc.edu/admin/exe/userApps.src.tgz && \
-                tar -xzf userApps.src.tgz && \
-                cd userApps && \
-                make && \
-                mv bin/wigToBigWig ../bin/wigToBigWig
-                cd $INSTALL_DIR
+            wget http://hgdownload.cse.ucsc.edu/admin/exe/userApps.src.tgz
+            tar -xzf userApps.src.tgz
+            cd userApps
+            make
+            mv bin/wigToBigWig ../bin/wigToBigWig
+            cd $INSTALL_DIR
         fi
         
           
