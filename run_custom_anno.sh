65fd4abc799114b03e3aabedfc00a514ca96807f
Geo Pertea <geo.pertea@gmail.com>
2023-02-19 19:47:52 -0500

    run.sh and run_custom_anno.sh templates updated

--- run_custom_anno.sh
+++ run_custom_anno.sh
@@ -9,23 +9,25 @@
 ## * wrk and results directories are going to be created
 ## * the output will be in ./results/
 
+module use /jhpce/shared/jhpce/modulefiles/libd
+module load nextflow/20.01.0
+
 ### === edit the following lines as needed:
 STRAND=reverse # "unstranded" , "forward" or "reverse"
-STYPE=paired    # "single" or "paired"
-RESUME=0        # change this to 1 if you want to resume interrupted run
-REF=hg38 # reference genome - can be hg38, hg19, mm10 or rn6
+STYPE=paired      # "single" or "paired"
+RESUME=0          # change this to 1 if you want to resume interrupted run
+##REF=hg38 # reference genome - can be hg38, hg19, mm10 or rn6
+##--- using custom humanRat reference & annotation:
+REF="humanRat"
+ANN=/dcs04/lieber/lcolladotor/dbDev_LIBD001/RNAseq/stemcell_pipeline/spqz_ref
 
 ### === only edit these if you want to change the default output/input directories
 OUTDIR=$(pwd -P)
 INDIR="$OUTDIR" # samples.manifest must be here
 
-#module use /jhpce/shared/jhpce/modulefiles/libd
-module load nextflow/20.01.0
 export _JAVA_OPTIONS="-Xms8g -Xmx10g"
 
 SPQZ=/dcl02/lieber/ajaffe/gpertea/spqz
-#SPQZ=/dcs04/lieber/lcolladotor/dbDev_LIBD001/RNAseq/SPEAQeasy
-## /dcl01/lieber/ajaffe/Nick/SPEAQeasy
 SPLOG=$PWD/SPEAQeasy_output.log
 
 mkdir -p "$OUTDIR/wrk"
@@ -34,19 +36,20 @@ optResume=""
 if [[ $RESUME == 1 || $RESUME == yes ]]; then
  optResume="-resume"
 fi
-## use the Annotation directly from Nick's installation?
-## ANN="/dcl01/lieber/ajaffe/Nick/SPEAQeasy/Annotation"
-ANN="$SPQZ/Annotation"
-## for local: nextflow -bg -Dnxf.pool.type=sync run $SPQZ/main.nf
+## local run: nextflow -bg -Dnxf.pool.type=sync run $SPQZ/main.nf 
 nextflow $SPQZ/main.nf \
     --sample $STYPE \
-    --reference $REF \
+    --annotation "$ANN" \
+    --custom_anno "$REF" \
+    --reference "$REF" \
     --strand $STRAND \
     --trim_mode adaptive \
-    --annotation "$ANN" \
     --input  "$INDIR" \
          -w  "$OUTDIR/wrk" \
     --output "$OUTDIR/results" \
     -with-report execution_reports/pipeline_report.html \
     -profile jhpce $optResume
-## for local: -profile local $optResume
+##        or: -profile jhpceM $optResume
+## local run: -profile local $optResume
+##        or: -profile localMk60 $optResume
+
